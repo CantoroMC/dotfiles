a342232a15eb78c8894cbc47031f8d4ce2fab4c5
Marco Cantoro <marco.cantoro92@outlook.it>
2020-12-21 13:31:19 +0100

    Rethinking

--- emacs/.config/emacs/emacs_custom.el
+++ emacs/.config/emacs/emacs_custom.el
@@ -1,1 +1,15 @@
 ;; Emacs Auto-Customization File
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(indicate-empty-lines t)
+ '(package-selected-packages
+   '(haskell-mode markdown-mode org-bullets org-plus-contrib all-the-icons-ivy-rich counsel ivy command-log-mode company rainbow-delimiters use-package undo-tree key-chord evil-surround evil-commentary evil-collection diminish auto-package-update auto-compile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
