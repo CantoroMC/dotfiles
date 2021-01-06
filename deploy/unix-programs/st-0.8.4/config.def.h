9d169210d3643f16f68ecde543b8b8772817ab9b
Marco Cantoro <marco.cantoro92@outlook.it>
2021-01-06 14:09:57 +0100

    luke smith unique features for st

--- deploy/unix-programs/st-0.8.4/config.def.h
+++ deploy/unix-programs/st-0.8.4/config.def.h
@@ -353,6 +353,10 @@ static MouseShortcut mshortcuts[] = {
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
+static char *openurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -o", "externalpipe", NULL };
+static char *copyurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -c", "externalpipe", NULL };
+static char *copyoutput[] = { "/bin/sh", "-c", "st-copyout", "externalpipe", NULL };
+
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,       {.i =  0} },
@@ -372,6 +376,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,         {.i =  0} },
 	{ TERMMOD,              XK_Return,      newterm,         {.i =  0} },
 	{ TERMMOD,              XK_space,       keyboard_select, {.i =  0} },
+	{ MODKEY,               XK_l,           externalpipe,    {.v = openurlcmd } },
+	{ MODKEY,               XK_y,           externalpipe,    {.v = copyurlcmd } },
+	{ MODKEY,               XK_o,           externalpipe,    {.v = copyoutput } },
 	// scroll-back
 	{ ShiftMask,            XK_Page_Up,     kscrollup,       {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,     {.i = -1} },
