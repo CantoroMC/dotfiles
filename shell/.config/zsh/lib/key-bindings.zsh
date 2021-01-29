144624e263bf0bf2a08ec4ea22de0319e972d18f
Marco Cantoro <marco.cantoro92@outlook.it>
2021-01-29 01:52:21 +0100

    fzf-git widgets for zsh

--- shell/.config/zsh/lib/key-bindings.zsh
+++ shell/.config/zsh/lib/key-bindings.zsh
@@ -30,9 +30,7 @@ key[Control-Delete]="${terminfo[kDC5]}"
 
 bindkey -v
 
-# Widgets {{{2
-
-# Native {{{3
+# Native Widgets {{{2
 autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N down-line-or-beginning-search
 zle -N up-line-or-beginning-search
@@ -41,28 +39,7 @@ autoload -Uz edit-command-line
 zle -N edit-command-line
 # }}}
 
-# Directory Navigation {{{3
-_cdUndoDir() {
-  popd
-  zle reset-prompt
-  print
-  ls
-  zle reset-prompt
-}
-
-_cdParentKey() {
-  pushd ..
-  zle reset-prompt
-  print
-  ls
-  zle reset-prompt
-}
-
-zle -N _cdUndoDir
-zle -N _cdParentKey
-# }}}
-
-# Commands: {{{3
+# Most used commands -> Widgets: {{{2
 _exit-cmd () {
   zle clear-screen
   bye
@@ -89,8 +66,6 @@ _nvim_cmd () {
   zle accept-line
 }
 zle -N _nvim_cmd
-# }}}
-
 # }}}
 
 # Kzbd compatible {{{2
@@ -131,41 +106,42 @@ bindkey -M vicmd "^[3;5~" delete-char
 # }}}
 
 # Ctrl+Key {{{2
-bindkey -M viins    '^A'    _tmux_cmd
-bindkey -M viins    '^B'    exchange-point-and-mark
-bindkey -M viins    '^E'    expand-cmd-path
-bindkey -M viins    '^F'    accept-and-infer-next-history
-bindkey -M viins    '^K'    vi-quoted-insert
-bindkey -M viins    '^G'    menu-complete
-# bindkey -M viins    '^I'    fzf-tab-complete
-bindkey -M viins    '^L'    clear-screen
-bindkey -M viins    '^N'    history-substring-search-down
-bindkey -M viins    '^P'    history-substring-search-up
-bindkey -M viins    '^Q'    _exit-cmd
-bindkey -M vicmd    '^Q'    _exit-cmd
-# bindkey -M viins    '^R'    fzf-history-widget
-# bindkey -M viins    '^T'    fzf-file-widget
-bindkey -M viins    '^V'    edit-command-line
-bindkey -M vicmd    '^V'    edit-command-line
-bindkey -M viins    '^Y'    copy-prev-shell-word
-bindkey -M viins    ' '     magic-space
-bindkey -M viins    '^B^N' _cdUndoDir
-bindkey -M vicmd    '^B^N' _cdUndoDir
-bindkey -M viins    '^B^M' _cdParentKey
-bindkey -M vicmd    '^B^M' _cdParentKey
-bindkey -M viins    '^X^L' _nnn_cmd
-bindkey -M vicmd    '^X^L' _nnn_cmd
-bindkey -M viins    '^X^E' _nvim_cmd
-bindkey -M vicmd    '^X^E' _nvim_cmd
-bindkey -M viins -s '^X^M' 'fzf-man\n'
-bindkey -M vicmd -s '^X^M' 'fzf-man\n'
-bindkey -M viins -s '^X^F' 'rcd\n'
-bindkey -M vicmd -s '^X^F' 'rcd\n'
-bindkey -M viins -s '^X^N' 'rcn\n'
-bindkey -M vicmd -s '^X^N' 'rcn\n'
+bindkey -M viins      '^A'    _tmux_cmd
+bindkey -M viins      '^B'    exchange-point-and-mark
+bindkey -M viins      '^E'    expand-cmd-path
+bindkey -M viins      '^F'    accept-and-infer-next-history
+bindkey -M viins      '^K'    vi-quoted-insert
+bindkey -M viins      '^G'    menu-complete
+# bindkey -M viins      '^I'    fzf-tab-complete
+bindkey -M viins      '^L'    clear-screen
+bindkey -M viins      '^N'    history-substring-search-down
+bindkey -M menuselect '^O'    accept-and-infer-next-history
+bindkey -M viins      '^P'    history-substring-search-up
+bindkey -M viins      '^Q'    _exit-cmd
+bindkey -M vicmd      '^Q'    _exit-cmd
+# bindkey -M viins      '^R'    fzf-history-widget
+# bindkey -M viins      '^T'    fzf-file-widget
+bindkey -M viins      '^V'    edit-command-line
+bindkey -M vicmd      '^V'    edit-command-line
+bindkey -M viins      '^Y'    copy-prev-shell-word
+bindkey -M viins      ' '     magic-space
+bindkey -M viins      '^B^N' _cdUndoDir
+bindkey -M vicmd      '^B^N' _cdUndoDir
+bindkey -M viins      '^B^M' _cdParentKey
+bindkey -M vicmd      '^B^M' _cdParentKey
+bindkey -M viins      '^X^L' _nnn_cmd
+bindkey -M vicmd      '^X^L' _nnn_cmd
+bindkey -M viins      '^X^E' _nvim_cmd
+bindkey -M vicmd      '^X^E' _nvim_cmd
+bindkey -M viins -s   '^X^M' 'fzf-man\n'
+bindkey -M vicmd -s   '^X^M' 'fzf-man\n'
+bindkey -M viins -s   '^X^F' 'rcd\n'
+bindkey -M vicmd -s   '^X^F' 'rcd\n'
+bindkey -M viins -s   '^X^N' 'rcn\n'
+bindkey -M vicmd -s   '^X^N' 'rcn\n'
 # }}}
 
-# Alt-Key {{{2
+# Alt-Key: Emacs Like {{{2
 # bindkey -M viins    '\ec'  fzf-cd-widget
 bindkey -M viins    '\ea'  beginning-of-line
 bindkey -M viins    '\eb'  backward-word
@@ -211,96 +187,24 @@ bindkey -M visual S add-surround
 
 # }}}
 
-# Additional fzf + git integration {{{2
+# Fzf integration {{{2
+autoload fzf-git fzf-bindkey fzf-kill
 
-# Auxiliary functions {{{3
-is_in_git_repo() {
-  git rev-parse HEAD > /dev/null 2>&1
-}
-
-fzf-down() {
-  fzf --height 50% "$@" --border
-}
-
-join-lines() {
-  local item
-  while read item; do
-    echo -n "${(q)item}"
-  done
-}
-
-bind-git-helper() {
-  local c
-  for c in $@; do
-    eval "fzf-g$c-widget() { local result=\$(_g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
-    eval "zle -N fzf-g$c-widget"
-    eval "bindkey '^g^$c' fzf-g$c-widget"
-  done
-}
-
-# }}}
-
-# Widgets: {{{3
-_gf() {
-  is_in_git_repo || return
-  git -c color.status=always status --short |
-    fzf-down -m --ansi --nth 2..,.. \
-    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
-    cut -c4- | sed 's/.* -> //'
-  }
-
-_gb() {
-  is_in_git_repo || return
-  git branch -a --color=always | grep -v '/HEAD\s' | sort |
-    fzf-down --ansi --multi --tac --preview-window right:70% \
-    --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
-    sed 's/^..//' | cut -d' ' -f1 |
-    sed 's#^remotes/##'
-  }
-
-_gt() {
-  is_in_git_repo || return
-  git tag --sort -version:refname |
-    fzf-down --multi --preview-window right:70% \
-    --preview 'git show --color=always {} | head -'$LINES
-  }
-
-_go() {
-  is_in_git_repo || return
-  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-    fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
-    --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
-    grep -o "[a-f0-9]\{7,\}"
-  }
-
-_gr() {
-  is_in_git_repo || return
-  git remote -v | awk '{print $1 "\t" $2}' | uniq |
-    fzf-down --tac \
-    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
-    cut -d$'\t' -f1
-  }
-# }}}
-
-bind-git-helper f b t o r
-
-unset -f bind-git-helper
-
-# }}}
-
-# More fzf integration {{{2
-autoload fzf-bindkey fzf-kill
-
+zle -N  fzf-git-branches fzf-git
+zle -N  fzf-git-files fzf-git
+zle -N  fzf-git-hashes fzf-git
+zle -N  fzf-git-remotes fzf-git
+zle -N  fzf-git-tags fzf-git
 zle -N fzf-bindkey
 zle -N fzf-kill
 
+bindkey -M viins '^G^B' fzf-git-branches
+bindkey -M viins '^G^F' fzf-git-files
+bindkey -M viins '^G^H' fzf-git-hashes
+bindkey -M viins '^G^R' fzf-git-remotes
+bindkey -M viins '^G^T' fzf-git-tags
 bindkey -M viins '^X^B' fzf-bindkey
 bindkey -M viins '^X^K' fzf-kill
-# }}}
-
-# MenuSelect Map {{{2
-bindkey -M menuselect '^o' accept-and-infer-next-history
 # }}}
 
 # }}}
