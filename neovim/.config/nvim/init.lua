bfd2a4568a8879becc4a8c1a762543d7263761d9
Marco Cantoro <marco.cantoro92@outlook.it>
2021-04-10 05:02:52 +0200

    xmenu utilities, gitsigns has a bug

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -20,7 +20,7 @@ require'mc.plugin.octolua'.digest()
 
 -- COLORSCHEME: managed by a small lua script
 require'mc.plugin.colorpicker'.setup {
-  active = false,
+  active = true,
   theme = { [ "ayu" ] = 'ayu_dark'},
   themes     = {
     [ "dark" ]  = {
--- neovim/.config/nvim/lua/mc/plugin/config/gitsigns.lua
+++ neovim/.config/nvim/lua/mc/plugin/config/gitsigns.lua
@@ -1,29 +1,55 @@
 require('gitsigns').setup {
   signs = {
-    add          = {hl = 'DiffAdd'   , text = '│', numhl='GitSignsAddNr'},
-    change       = {hl = 'DiffChange', text = '│', numhl='GitSignsChangeNr'},
-    delete       = {hl = 'DiffDelete', text = '_', numhl='GitSignsDeleteNr'},
-    topdelete    = {hl = 'DiffDelete', text = '‾', numhl='GitSignsDeleteNr'},
-    changedelete = {hl = 'DiffChange', text = '~', numhl='GitSignsChangeNr'},
+    add = {
+      hl = 'GitSignsAdd',
+      text = '│',
+      numhl='GitSignsAddNr',
+      linehl='GitSignsAddLn'
+    },
+    change = {
+      hl = 'GitSignsChange',
+      text = '│',
+      numhl='GitSignsChangeNr',
+      linehl='GitSignsChangeLn'
+    },
+    delete = {
+      hl = 'GitSignsDelete',
+      text = '_',
+      numhl='GitSignsDeleteNr',
+      linehl='GitSignsDeleteLn'
+    },
+    topdelete = {
+      hl = 'GitSignsDelete',
+      text = '‾',
+      numhl='GitSignsDeleteNr',
+      linehl='GitSignsDeleteLn'
+    },
+    changedelete = {
+      hl = 'GitSignsChange',
+      text = '~',
+      numhl='GitSignsChangeNr',
+      linehl='GitSignsChangeLn'
+    },
   },
-  numhl = true,
+  numhl = false,
+  linehl = false,
   keymaps = {
     -- Default keymap options
     noremap = true,
-    buffer = true,
-
+    buffer  = true,
+    -- motions
     ['n ]c'] = { expr = true, "&diff ? ']c' : '<Cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
     ['n [c'] = { expr = true, "&diff ? '[c' : '<Cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
-
-    ['n <Leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['n <Leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <Leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['n <Leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n <Leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-
     -- Text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-    ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
+    ['o ig'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
+    ['x ig'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
+    -- git actions
+    ['n <C-C><C-H><C-S>'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['n <C-C><C-H><C-U>'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <C-C><C-H><C-R>'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['n <C-C><C-H><C-P>'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <C-C><C-H><C-B>'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+
   },
   watch_index = {
     interval = 1000
