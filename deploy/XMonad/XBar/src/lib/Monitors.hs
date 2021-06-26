9c977959e3473b3d36084822f5f967e8b0532046
Marco Cantoro <marco.cantoro92@outlook.it>
2021-06-26 02:32:01 +0200

    up

--- deploy/XMonad/XBar/src/lib/Monitors.hs
+++ deploy/XMonad/XBar/src/lib/Monitors.hs
@@ -41,7 +41,7 @@ import Util
 -------------------------------------------------------------------------------
     -- Commands
 trayerPad :: Command
-trayerPad = Com "/bin/sh" ["-c", xBarConfigDir ++ "/utilities/scripts/padding-icon"] "trayerPad" 60
+trayerPad = Com "/bin/sh" ["-c", xBarConfigDir ++ "/utilities/scripts/padding-icon"] "trayerPad" 600
 
 pacman :: Command
 pacman = Com "/bin/sh" ["-c", xBarConfigDir ++ "/utilities/scripts/xmPacman"] "pacman" 600
